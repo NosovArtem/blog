1ee2c0a6b733a513388f9881f3abc8a3350bf029
Artem <nosov.a.o@yandex.ru>
2021-05-24 23:17:03 +0300

    add bootstrap

--- src/init_db/start.sql
+++ src/init_db/start.sql
@@ -1,3 +1,23 @@
+CREATE TABLE message(
+        id int NOT NULL PRIMARY KEY,
+        filename CHAR (255),
+        tag CHAR (255),
+        text CHAR (255),
+        user_id int
+);
+
+CREATE TABLE usr(
+        id int NOT NULL PRIMARY KEY,
+        active BOOLEAN NOT NULL,
+        password char (255),
+        username char (255)
+);
+
+CREATE TABLE user_role(
+        user_id int NOT NULL,
+        roles char (255)
+);
+
 INSERT INTO usr VALUES (1, true, 'u', 'u');
 INSERT INTO usr VALUES (2, true, 'usr', 'usr');
 INSERT INTO usr VALUES (3, true, 'a', 'a');
@@ -8,6 +28,7 @@ INSERT INTO user_role VALUES (1, 'USER');
 INSERT INTO user_role VALUES (2, 'USER');
 INSERT INTO user_role VALUES (3, 'ADMIN');
 INSERT INTO user_role VALUES (4, 'ADMIN');
-INSERT INTO user_role VALUES (5, 'USER,ADMIN');
+INSERT INTO user_role VALUES (5, 'USER');
+INSERT INTO user_role VALUES (5, 'ADMIN');
 
 commit;
