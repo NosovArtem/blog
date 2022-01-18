de6a380a6a961fb6984d9179676c3cb0675bdfd0
Artem <nosov.a.o@yandex.ru>
2022-01-18 21:36:52 +0300

    - added  migration for DB - added view for edit user

--- src/main/java/com/nosov/blog/controller/UserController.java
+++ src/main/java/com/nosov/blog/controller/UserController.java
@@ -2,33 +2,32 @@ package com.nosov.blog.controller;
 
 import com.nosov.blog.domain.Role;
 import com.nosov.blog.domain.User;
-import com.nosov.blog.repository.UserRepo;
+import com.nosov.blog.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
 import java.util.Map;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("/user")
-@PreAuthorize("hasAuthority('ADMIN')") // каждый мапинг будет проверять роль пользователя. (Доступны только для ADMIN)
 public class UserController {
 
     @Autowired
-    UserRepo userRepo;
+    UserService userService;
 
     @GetMapping
+    @PreAuthorize("hasAuthority('ADMIN')")//(Доступ только для ADMIN)
     public String userList(Model model) {
-        model.addAttribute("users", userRepo.findAll());
+        model.addAttribute("users", userService.findAll());
         return "userList";
     }
 
     @GetMapping("{user}")
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userEditForm(@PathVariable User user, Model model) {
         model.addAttribute("user", user);
         model.addAttribute("roles", Role.values());
@@ -36,23 +35,28 @@ public class UserController {
     }
 
     @PostMapping
+    @PreAuthorize("hasAuthority('ADMIN')")
     public String userSave(@RequestParam String username, @RequestParam Map<String, String> form, @RequestParam("userId") User user) {
-        user.setUsername(username);
-
-        Set<String> roles = Arrays.stream(Role.values())
-                .map(Role::name)
-                .collect(Collectors.toSet());
-
-        user.getRoles().clear();
-
-        for (String key : form.keySet()) {
-            if (roles.contains(key)) {
-                user.getRoles().add(Role.valueOf(key));
-            }
-
-        }
-
-        userRepo.save(user);
+        userService.saveUser(user, username, form);
         return "redirect:/user";
+    }
+
+    @GetMapping("profile")
+    public String getProfile(Model model, @AuthenticationPrincipal User user) {
+        model.addAttribute("username", user.getUsername());
+        model.addAttribute("email", user.getEmail());
+
+        return "profile";
+    }
+
+    @PostMapping("profile")
+    public String updateProfile(
+            @AuthenticationPrincipal User user,
+            @RequestParam String email,
+            @RequestParam String password
+    ) {
+        userService.updateProfile(user, email, password);
+
+        return "redirect:/user/profile";
     }
 }
--- src/main/java/com/nosov/blog/service/UserService.java
+++ src/main/java/com/nosov/blog/service/UserService.java
@@ -11,8 +11,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-import java.util.Collections;
-import java.util.UUID;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements UserDetailsService {
@@ -44,19 +44,23 @@ public class UserService implements UserDetailsService {
         userRepo.save(user);
 
         if (!StringUtils.isEmpty(user.getEmail())) {
-            String message = String.format(
-                    "Hello %s! \n " +
-                            "Wellcome to Blog. " +
-                            "Please, visit next link: %s/activate/%s",
-                    user.getUsername(),
-                    blogUrl,
-                    user.getActivationCode()
-            );
-
-            mailSender.send(user.getEmail(), "Activation code", message);
+            sendActivationCode(user);
         }
 
         return true;
+    }
+
+    private void sendActivationCode(User user) {
+        String message = String.format(
+                "Hello %s! \n " +
+                        "Wellcome to Blog. " +
+                        "Please, visit next link: %s/activate/%s",
+                user.getUsername(),
+                blogUrl,
+                user.getActivationCode()
+        );
+
+        mailSender.send(user.getEmail(), "Activation code", message);
     }
 
     public boolean activateUser(String code) {
@@ -67,5 +71,47 @@ public class UserService implements UserDetailsService {
         user.setActive(true);
         userRepo.save(user);
         return true;
+    }
+
+    public void saveUser(User user, String username, Map<String, String> form) {
+        user.setUsername(username);
+
+        Set<String> roles = Arrays.stream(Role.values())
+                .map(Role::name)
+                .collect(Collectors.toSet());
+
+        user.getRoles().clear();
+
+        for (String key : form.keySet()) {
+            if (roles.contains(key)) {
+                user.getRoles().add(Role.valueOf(key));
+            }
+
+        }
+        userRepo.save(user);
+    }
+
+    public List<User> findAll() {
+        return userRepo.findAll();
+    }
+
+    public void updateProfile(User user, String email, String password) {
+        String userEmail = user.getEmail();
+
+        boolean isEmailChanged = (userEmail != null && !userEmail.equals(email))
+                || (email != null && !email.equals(userEmail));
+        if (isEmailChanged) {
+            user.setEmail(email);
+        }
+
+        if (!StringUtils.isEmpty(password)) {
+            user.setPassword(password);
+        }
+
+        userRepo.save(user);
+
+        if (isEmailChanged) {
+            sendActivationCode(user);
+        }
     }
 }
